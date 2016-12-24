85c2543f234ce9fb3e6b9522ba964dc599bd12aa
Christian Dreier <flipper.x3r@googlemail.com>
2016-12-24 18:39:37 +0100

    Use Jinja to render a template

--- dirread.py
+++ dirread.py
@@ -2,8 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+import jinja2
+
+
 def main():
-    pass
+    with open("template.html", encoding="utf8") as f:
+        content = f.read()
+    template = jinja2.Template(content)
+    rendered = template.render(
+        path="Test path",
+        message="Hello, world!"
+    )
+    print(rendered)
 
 
 if __name__ == '__main__':
